40918db6d5ce99dabd74d30cf2873c521e7b0e63
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-07-24 11:54:49 +0200

    Use browser()

--- R/process_chart.R
+++ R/process_chart.R
@@ -87,7 +87,7 @@ process_chart <- function(individual,
                             exact_ga = exact_ga,
                             break_ties = break_ties)
   }
-  cat("matches: ", matches, "\n")
+  browser()
 
   # set the palette
   parsed <- parse_chartcode(chartcode)
