875c6efe9fcec7de1061ffc038a3152953ad022a
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-02-10 16:08:35 +0100

    Make testfile with 1997 refs

--- tests/testthat/test-curve_interpolation.R
+++ tests/testthat/test-curve_interpolation.R
@@ -15,20 +15,19 @@ data2 <- data.frame(
   hgt = c(52, 60, 78, 69, 70, NA, NA, NA),
   stringsAsFactors = FALSE)
 
-ref <- clopus::nl2009[["nl2009.mhgtNL"]]
+ref <- clopus::nl1997[["nl1997.mhgtNL"]]
+refcode <- "nl_1997_hgt_male_nl"
 
 test_that("handles id with single observations", {
-  expect_equal(c(52, 60.0, 68.4, 75.9, 78.0, 69.0, 62.0, 72.5, 78.0, 82.0),
-               curve_interpolation(data1, xname = "age", yname = "hgt", xout = seq(0, 1.3, 0.5),
-                                   reference = ref)$hgt,
-               tolerance = 0.001)
+  expect_equal(curve_interpolation(data1, xname = "age", yname = "hgt",
+                                   xout = seq(0, 1.3, 0.5), reference = ref)$hgt,
+               c(52, 60, 68.0758131496374, 75.6616971524598, 78, 69, 62, 72.7247949352105, 78, 82))
   })
 
 test_that("handles id with all missing outcome measurements", {
-  expect_equal(c(52, 60.0, 68.4, 75.9, 78.0, 69.0, 70.0, NA, NA),
-               curve_interpolation(data2, xname = "age", yname = "hgt", xout = seq(0, 1.3, 0.5),
-                                   reference = ref)$hgt,
-               tolerance = 0.001)
+  expect_equal(curve_interpolation(data2, xname = "age", yname = "hgt",
+                                   xout = seq(0, 1.3, 0.5), reference = ref)$hgt,
+               c(52, 60, 68.0758131496374, 75.6616971524598, 78, 69, 70, NA, NA))
 })
 
 xyz <- data.frame(x = numeric(0), y = numeric(0))
