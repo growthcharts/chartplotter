734dd46310b6150bb544921fd98380c10b00abbc
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-02-24 15:26:08 +0100

    Make length of yout compatible to xout

--- R/internal.R
+++ R/internal.R
@@ -52,7 +52,9 @@ restore_factors <- function(data, f = NULL) {
 safe_approx <- function (x, y = NULL, xout, method = "linear", n = 50,
                          rule = 1, f = 0, ties = mean, na.rm = TRUE)
   {
-  if (!length(x)) return(list(x = xout, y = numeric(0)))
+  if (!length(x)) {
+    return(list(x = xout, y = rep(NA_real_, length(xout))))
+  }
   approx(x = x, y = y, xout = xout, method = method, n = n,
          rule = rule, f = f, ties = ties, na.rm = na.rm)
   }
