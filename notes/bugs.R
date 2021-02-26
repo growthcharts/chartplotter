8a41499bf1b3137dd904932d80fa9cdd9ca120d1
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-02-26 12:52:43 +0100

    Update buglist

--- notes/bugs.R
+++ notes/bugs.R
@@ -1,12 +1,12 @@
 Leeg scherm geeft server error - OPGELOST 26feb21
 T3254/T2035/T6021 geven geen lengte naar leeftijd / maar wel als predictor aan staat - OPGELOST 25feb21
 curve 1 punt wordt niet geplot - OPGELOST 25feb21
-geboortegewicht/lengte wordt niet geplot
 Laura D-score geeft server error OPGELOST 26feb21
 hdc 4-18 predictiemodel bestaat niet --> server error OPGELOST 26feb21
-Nederland 2010 - 0-4 jaar - Gewicht naar leeftijd - keuze Turks/Mar/Hin --> TJEW ontbreekt - OPGELOST 26feb21
+Nederland 2010 - 0-4 jaar - Gewicht naar leeftijd - keuze Turks/Mar/Hin --> "TJEW ontbreekt" - OPGELOST 26feb21
 
 Curve interpolation for DSC 0-4 jaar werkt niet - OPGELOST 26feb21
 Predicties voor DSC boven 24m niet toestaan - OPGELOST 26feb21
 
-{"txt":"","loc":"http://localhost/ocpu/tmp/x075f3ca338513f/","chartcode":"","selector":"derive","chartgrp":"nl2010","agegrp":"0-15m","sex":"male","etn":"nl","ga":25,"side":"dsc","curve_interpolation":true,"quiet":false,"dnr":"2-4","lo":"36m","hi":"45m","nmatch":0,"exact_sex":true,"exact_ga":false,"show_future":false,"show_realized":false}
+Geboortegewicht/lengte wordt niet geplot
+Gewicht naar lengte doet geen curve interpolation
