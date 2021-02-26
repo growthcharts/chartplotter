0be4b78cc43d4a264247cfa42fb30d69cd7d128e
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-02-26 13:06:44 +0100

    Update buglist

--- notes/bugs.R
+++ notes/bugs.R
@@ -1,12 +1,11 @@
-Leeg scherm geeft server error - OPGELOST 26feb21
 T3254/T2035/T6021 geven geen lengte naar leeftijd / maar wel als predictor aan staat - OPGELOST 25feb21
 curve 1 punt wordt niet geplot - OPGELOST 25feb21
+Leeg scherm geeft server error - OPGELOST 26feb21
 Laura D-score geeft server error OPGELOST 26feb21
 hdc 4-18 predictiemodel bestaat niet --> server error OPGELOST 26feb21
 Nederland 2010 - 0-4 jaar - Gewicht naar leeftijd - keuze Turks/Mar/Hin --> "TJEW ontbreekt" - OPGELOST 26feb21
-
 Curve interpolation for DSC 0-4 jaar werkt niet - OPGELOST 26feb21
 Predicties voor DSC boven 24m niet toestaan - OPGELOST 26feb21
+Gewicht naar lengte doet geen curve interpolation - OPGELOST 26feb21
 
 Geboortegewicht/lengte wordt niet geplot
-Gewicht naar lengte doet geen curve interpolation
